152489bad17b7bd68dbad60cfd686fd5b6a7c069
Apekshavaity07 <98224195+Apekshavaity07@users.noreply.github.com>
2024-07-03 15:53:19 +0530

    Update Files

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-var Preloader = document.getElementById('loading');
+var reloader = document.getElementById("loading");
 
-function myfunction(){
-    Preloader.style.display = 'none';
-}
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
 
-var Active = document.getElementById("Active");
-        var activeBtn = Active.getElementsByClassName("active-btn");
-        for (var i = 0; i < activeBtn.length; i++) {
-            activeBtn[i].addEventListener("click", function () {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += "active";
-            });
-        }
+
+/*var scroll = new smoothScroll('a[href*="#]',{
+    speed :1000,
+    speedDuration: true});*/
